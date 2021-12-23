e25400bd0be8c8c07b0da49f2c524901a9b0cbe9
ness <qE6@5K0!q>
2021-12-23 13:56:32 -0500

    Server Management Complete | Data Management is kept in check

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const { connectionString } = require('./config.json');
 const logSchema = require('./messageLogSchema');
 const delSchema = require('./deleteLogSchema');
 
-
 // Client Creation
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
@@ -29,12 +28,13 @@ client.once('ready', async client => {
     })
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
     new logSchema({
         primedMessage: message.content,
         primedUser: message.author.username,
         time: Date.now()
       }).save()
+    await logSchema.deleteOne({}).sort({time: 1}).limit(1)
  });
 
 client.on('messageDelete', message => {
@@ -66,11 +66,22 @@ client.on('interactionCreate', async interaction => {
     if (commandName === 'ping') {
         await interaction.reply('Pong');
     } else if (commandName === 'snipe') {
-        const results = await delSchema.find({})
+        // Retrieve Function [Sort]
+        const results = await delSchema.find({
+            delId: ' '
+        })
             .sort({
                 time: -1
             })
-        console.log('RESULTS', results)
+            .limit(1)
+        console.log(results)
+        // Delete Function [Data Management]
+        const deleteOneLog = await delSchema.deleteOne({}).sort({
+            time: 1
+        })
+            .limit(1)
+        console.log(deleteOneLog)
+        interaction.reply(`The message you have sniped is ${delId}`)
     }
 });
 
